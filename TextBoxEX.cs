bd61da63601fd7816c116de994813b38974a5e7e
finedb <fukada@peace-create.com>
2014-01-06 17:54:35 +0900

    ネームスペース追加

--- TextBoxEX.cs
+++ TextBoxEX.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 // 2014-01-06
 // 【注意】Application.EnableVisualStyles(); が無効の時はwatertextが機能しない。
 
-namespace Aa.ControlEx
+namespace Aa.API.ControlEx
 {
 
   /// <summary>
